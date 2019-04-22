cde78aa834d068c4e251c132f1ae5fdeab5eae0d
Abdulaziz Al Zaabi <alzaabi98@gmail.com>
2019-04-22 11:17:46 +0400

    added error flash for login form

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(flash());
 app.use((req, res, next) => {
     res.locals.successMessage = req.flash('successMessage')
     res.locals.errorMessage = req.flash('errorMessage')
+    res.locals.error = req.flash('error');
     next()
 })
 
